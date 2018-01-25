81f9146effefebcec6a7c3caba010daca4d08836
Leo Hussain <leo.94@hotmail.co.uk>
2018-01-25 08:26:10 -0500

    ChangeDIV v2

--- uceshus/www/js/appActivity.js
+++ uceshus/www/js/appActivity.js
@@ -1,3 +1,9 @@
 function menuClicked() {
 alert("You clicked the menu");
 }
+
+function replaceGraphs() {
+	var img = document.createElement("IMG");
+	img.src = "images/ucl.png";
+	document.getElementById('graphdiv').replaceChild(img, graphdiv)
+}
